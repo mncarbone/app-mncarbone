70535cba00578da4ede3efc2f106ac7fcfbd7832
mncarbone <mncarbone@gmail.com>
2017-08-25 20:04:07 -0300

    Cambio en las variables para OpenShift3

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
 $sql = $_REQUEST['sql'];
 
-$dbhost = getenv('OPENSHIFT_MYSQL_DB_HOST'); // Host name
-$dbport = getenv('OPENSHIFT_MYSQL_DB_PORT'); // Host port
-$dbusername = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); // MySQL username
-$dbpassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD'); // MySQL password
-$dbname = getenv('OPENSHIFT_GEAR_NAME'); // Database name
+$dbhost = getenv('MYSQL_SERVICE_HOST'); // Host name
+$dbport = getenv('MYSQL_SERVICE_PORT'); // Host port
+$dbusername = getenv('MYSQL_USER'); // MySQL username
+$dbpassword = getenv('MYSQL_PASSWORD'); // MySQL password
+$dbname = getenv('MYSQL_DATABASE'); // Database name
 
 $conn = mysqli_connect($dbhost, $dbusername, $dbpassword);
 mysqli_select_db($conn, $dbname);
